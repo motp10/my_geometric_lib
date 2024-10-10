a2598bebce6fe27b284bda078db694187ac9900d
motp10 <104691429+motp10@users.noreply.github.com>
2024-10-10 08:40:17 +0300

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,12 @@
+import math
+
+
+def area(r):
+'''принимает радиус r возвращает площадь круга pi*r*r'''
+    return math.pi * r * r
+
+
+def perimeter(r):
+'''принимает радиус r возвращает длинну окружности 2*pi*r'''
+    return 2 * math.pi * r
+
